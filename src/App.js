f3c6064d142132e9bda2f621a318acdee8f1f27c
Anand Shukla <114645060+ansh12500@users.noreply.github.com>
2023-10-07 17:07:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import StudentTable from "./Component/StudentTable";
+import Navbar from "./Component/Navbar";
+
+
+
+function App() {
+  return (
+    <>
+  
+       <Navbar />
+      <StudentTable />
+      
+    </>
+       
+    
+   
+  );
+}
+
+export default App;
